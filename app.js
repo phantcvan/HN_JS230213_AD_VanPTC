9b5ebc6642333f47c2714af90bfa63e5f2093e9d
phantcvan <125523657+phantcvan@users.noreply.github.com>
2023-05-19 10:44:32 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ server.use("/api/v1/users", usersRoutes)
 server.use("/api/v1/posts", postsRoutes)
 
 
-
+// Khai báo middleware xử lý trang 404
+server.use((req, res, next) => {
+    res.status(404).send('<h1>PAGE NOT FOUND</h1>');
+});
 
 // Cài đặt để server luôn chờ đợi và lắng nghe các request gửi lên từ client
 server.listen(3000, () => {
